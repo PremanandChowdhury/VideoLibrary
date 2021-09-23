aad0de3d3d09bbfffef353fd27b8fbc8bc4a54da
PremanandChowdhury <freudigprem@gmail.com>
2021-09-24 01:20:38 +0530

    code: Added the API call to the youtube

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBar from './SearchBar'
+import youtube from './apis/youtube'
 
 const App = () => {
+  const [videos, setVideos] = useState([])
+
+  const onSearchSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+      },
+    })
+    setVideos(response.data.items)
+  }
+
   return (
     <div className='ui container'>
-      <SearchBar />
+      <SearchBar onSearchSubmit={onSearchSubmit} />
     </div>
   )
 }
