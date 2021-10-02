0f4b12cfd6be6ef0333b388e5dd980c9d7e42160
PremanandChowdhury <freudigprem@gmail.com>
2021-10-02 23:23:15 +0530

    note: Document added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react'
 import SearchBar from './SearchBar'
 import youtube from './apis/youtube'
+import VideoList from './VideoList'
 
 const App = () => {
   const [videos, setVideos] = useState([])
+
+  /**
+   * Here the youtube is:
+   *  -> https://www.googleapis.com/youtube/v3
+   * 
+   * Full URL will be
+   *  -> https://www.googleapis.com/youtube/v3/search?q=term
+   */
 
   const onSearchSubmit = async (term) => {
     const response = await youtube.get('/search', {
@@ -17,6 +26,7 @@ const App = () => {
   return (
     <div className='ui container'>
       <SearchBar onSearchSubmit={onSearchSubmit} />
+      <VideoList videos={videos} />
     </div>
   )
 }
