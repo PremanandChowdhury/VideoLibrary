8314267f0a828f433188053cf6050a9bda56d408
PremanandChowdhury <freudigprem@gmail.com>
2021-10-02 23:23:54 +0530

    feat: VideoList Component created

---
+++ src/components/VideoList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import VideoItem from './VideoItem'
+
+const VideoList = ({ videos }) => {
+  const renderedList = videos.map((video) => <VideoItem video={video} />)
+
+  return <div>{renderedList}</div>
+}
+
+export default VideoList
