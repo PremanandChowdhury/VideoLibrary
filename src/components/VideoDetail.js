ae57fc13be501afcd4407c3d22d1cc29ec5b082b
PremanandChowdhury <freudigprem@gmail.com>
2021-10-04 23:56:29 +0530

    code: Video Title Description section

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
 const VideoDetail = ({ video }) => {
-  if (!video) return <div>Loading...</div>
-  return <div>{video.snippet.title}</div>
+  if (!video) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+      <div className='ui segment'>
+        <h4 className='ui header'>{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  )
 }
 
 export default VideoDetail
