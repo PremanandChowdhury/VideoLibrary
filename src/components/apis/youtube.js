106ab8942ecf24d6ad6985bbdb24e3effaf9ff1c
PremanandChowdhury <freudigprem@gmail.com>
2021-11-04 09:54:19 +0530

    fix: Added Environment Variable

--- src/components/apis/youtube.js
+++ src/components/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const KEY = 'AIzaSyAG7Tgu0Wn7vf29bhlikf2l18A8amA62SE'
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
